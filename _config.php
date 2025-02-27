2f75dc46fd13e70f4a97503293686f64078e0f3a
Anigone1 <support@afabra.in>
2025-02-27 11:45:15 +0600

    Update _config.php

--- _config.php
+++ _config.php
@@ -1,1 +1,30 @@
+<?php 
+
+$conn = new mysqli("coolify-database", "mysql", "pi8ckWdZZ41Ef90LQ11dUaZQD7JTyNu6QTwLDc305dj0gqcymUT2aqelXKK1Ikfq", "default");
+
+if ($conn->connect_error) {
+    error_log("Database connection failed: " . $conn->connect_error);
+    
+}
+
+$websiteTitle = "AniPaca";
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";
+$websiteLogo = $websiteUrl . "/public/logo/logo.png";
+$contactEmail = "@gmail.com";
+
+$version = "2";
+
+$discord = "https://dcd.gg/anipaca";
+$github = "https://github.com/PacaHat";
+$telegram = "https://t.me/anipaca";
+$instagram = "https://www.instagram.com/pxr15_";
+
+// all the api you need
+
+$api = "https://api.anigone.com/api/v2/hianime"; //https://github.com/ghoshRitesh12/aniwatch-api
+$zpi = "https://your-hosted-api.com/api"; //https://github.com/PacaHat/zen-api
+$proxy = "https://your-hosted-proxy.com/cors?url="; //https://github.com/shashstormer/m3u8_proxy-cors
+
+
+$banner = $websiteUrl . "/public/images/banner.png";
 
